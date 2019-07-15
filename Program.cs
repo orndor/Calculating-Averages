f3e9186a7d77322c15da39efac021c2b1e63e9aa
Rob Orndoff <rorndoff@gmail.com>
2019-07-15 19:14:39 -0400

    Advanced Complete.  Need to review for clean-up later.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// File name: Program.cs
+// Project Name: Calculating Averages
+// Author: Orndoff, Robert K.
+// Date created: 07/15/2019
+// Date last modified: 07/15/2019
+//
+// C#
+using System;
 
 namespace Calculating_Averages
 {
@@ -6,8 +13,17 @@ namespace Calculating_Averages
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---===Begin Basic Difficulty===---");
             int[] rawScores = Scores();
-            Console.WriteLine($"The score average is: {SumScores(rawScores) / 10}"); 
+            Console.WriteLine($"The score average is: {SumScores(rawScores) / 10}");
+
+            Console.WriteLine("---===Begin Intermediate Difficulty===---");
+            int[] advRawScores = AdvScores(new int[GetNumOfScores()]);
+            int advAverage = AdvSumScores(advRawScores) / advRawScores.Length; //Needed to store this result in a variable for reference in Advanced Difficulty code below.
+            Console.WriteLine($"The score average is: {advAverage}");
+
+            Console.WriteLine("---===Begin Advanced Difficulty===---");
+            LetterGrade(advAverage);
         }
         static int[] Scores()
         {
@@ -29,17 +45,55 @@ namespace Calculating_Averages
             Console.WriteLine($"The sum of all scores is: {sum}");
             return sum;
         }
-        //static void ScoreAverage(int sum)
-        //{
-        //    Console.WriteLine($"The average score is: {sum/10}");
-        //}
-        //static void PrintScores(int[] rawScores)
-        //{
-        //    foreach(int s in rawScores)
-        //    {
-        //        Console.WriteLine($"The scores are {s.ToString()}:");
-        //    }
-        //    return;
-        //}
+        static int GetNumOfScores()
+        {
+            Console.WriteLine($"How many scores are there?");
+            int numScores = Convert.ToInt32(Console.ReadLine());
+
+            return numScores;
+        }
+        static int[] AdvScores(int[] advScores)
+        {
+            for (int i = 0; i < advScores.Length; i++)
+            {
+                Console.WriteLine($"Enter score number {i + 1}:");
+                advScores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return advScores;
+        }
+        static int AdvSumScores(int[] advRawScores)
+        {
+            int advSum = 0;
+            foreach (int s in advRawScores)
+            {
+                advSum += s;
+            }
+            Console.WriteLine($"The sum of all scores is: {advSum}");
+            return advSum;
+        }
+        static void LetterGrade(int average)
+        {
+            if (average > 89)
+            {
+                Console.WriteLine($"The letter grade average is: A");
+            }
+            else if (average > 79 && average < 90)
+            {
+                Console.WriteLine($"The letter grade average is: B");
+            }
+            else if (average > 69 && average < 80)
+            {
+                Console.WriteLine($"The letter grade average is: C");
+            }
+            else if (average > 59 && average < 70)
+            {
+                Console.WriteLine($"The letter grade average is: D");
+            }
+            else
+            {
+                Console.WriteLine($"The letter grade average is: F");
+            }
+            return;
+        }
     }
 }
